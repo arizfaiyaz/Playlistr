852d597b83642709447ca5ddd253ed72e8fc02b7
arizfaiyaz <arizfaiyazwork@gmail.com>
2025-09-01 23:16:37 +0530

    chore: remove unnecessary blank lines in index.js

--- backend/index.js
+++ backend/index.js
@@ -6,11 +6,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 const app = express();
 
-
-
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     connectDB();
     console.log("Server is running on port 3000");
 });
+
